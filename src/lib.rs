1a47dc0071414ed8967b4b6234e26495dc9e497e
jaykchen@gmail.com <jaykchen@gmail.com>
2023-12-14 01:54:48 +0000

    add route to answer q

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use flowsnet_platform_sdk::logger;
-use openai_flows::{embeddings::EmbeddingsInput, OpenAIFlows};
+use openai_flows::{
+    chat::{ChatModel, ChatOptions},
+    embeddings::EmbeddingsInput,
+    OpenAIFlows,
+};
 use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::str;
@@ -20,6 +24,12 @@ async fn handler(_headers: Vec<(String, String)>, qry: HashMap<String, Value>, b
         .unwrap()
         .as_str()
         .unwrap_or("my-book");
+    let question = qry
+        .get("ask")
+        .unwrap()
+        .as_str()
+        .unwrap_or("what's the gist of the book");
+
     let vector_size = qry.get("vector_size").unwrap().as_str().unwrap();
     let vector_size: u64 = vector_size.parse().unwrap_or(1536);
     let mut id: u64 = 0;
@@ -112,6 +122,11 @@ async fn handler(_headers: Vec<(String, String)>, qry: HashMap<String, Value>, b
             send_success("Cannot upsert into database!");
         }
     }
+    if qry.contains_key("ask") {
+        if let Some(res) = get_answer(question, &collection_name).await {
+            send_success(&res);
+        }
+    }
 }
 
 fn send_success(body: &str) {
@@ -120,4 +135,99 @@ fn send_success(body: &str) {
         vec![(String::from("content-type"), String::from("text/html"))],
         body.as_bytes().to_vec(),
     );
+}
+
+async fn get_answer(question: &str, collection_name: &str) -> Option<String> {
+    let mut openai = OpenAIFlows::new();
+    openai.set_retry_times(3);
+    let question_vector = match openai
+        .create_embeddings(EmbeddingsInput::String(question.to_string()))
+        .await
+    {
+        Ok(r) => {
+            if r.len() < 1 {
+                log::error!("OpenAI returned no embedding for the question");
+            }
+            r[0].iter().map(|n| *n as f32).collect()
+        }
+        Err(e) => {
+            log::error!("OpenAI returned an error: {}", e);
+            panic!()
+        }
+    };
+
+    // Search for embeddings from the question
+    let p = PointsSearchParams {
+        vector: question_vector,
+        limit: 5,
+    };
+    let mut system_prompt_updated = String::from(
+        "You're an AI assistant specialized in answering questions about a preloaded book, here are the most relevant fragments pertaining to the user's question: ",
+    );
+    match search_points(collection_name, &p).await {
+        Ok(sp) => {
+            for p in sp.iter() {
+                log::debug!(
+                    "Received vector score={} and text={}",
+                    p.score,
+                    first_x_chars(
+                        p.payload
+                            .as_ref()
+                            .unwrap()
+                            .get("text")
+                            .unwrap()
+                            .as_str()
+                            .unwrap(),
+                        256
+                    )
+                );
+                let p_text = p
+                    .payload
+                    .as_ref()
+                    .unwrap()
+                    .get("text")
+                    .unwrap()
+                    .as_str()
+                    .unwrap();
+                if p.score > 0.75 && !system_prompt_updated.contains(p_text) {
+                    system_prompt_updated.push_str("\n");
+                    system_prompt_updated.push_str(p_text);
+                }
+            }
+        }
+        Err(e) => {
+            log::error!("Vector search returns error: {}", e);
+        }
+    }
+    // log::debug!("The prompt is {} chars starting with {}", system_prompt_updated.len(), first_x_chars(&system_prompt_updated, 256));
+
+    let co = ChatOptions {
+        // model: ChatModel::GPT4,
+        model: ChatModel::GPT35Turbo16K,
+        restart: false,
+        system_prompt: Some(&system_prompt_updated),
+        post_prompt: None,
+        ..Default::default()
+    };
+
+    match openai.chat_completion("chatid-99", &question, &co).await {
+        Ok(r) => Some(r.choice),
+
+        Err(e) => {
+            log::error!("OpenAI returns error: {}", e);
+            None
+        }
+    }
+}
+
+fn reply(s: &str) {
+    send_response(
+        200,
+        vec![(String::from("content-type"), String::from("text/html"))],
+        s.as_bytes().to_vec(),
+    );
+}
+
+fn first_x_chars(s: &str, x: usize) -> String {
+    s.chars().take(x).collect()
 }
