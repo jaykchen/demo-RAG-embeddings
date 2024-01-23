a18491ccbe9ea5585f218a3a19b056380711fde2
jaykchen@gmail.com <jaykchen@gmail.com>
2024-01-22 20:47:50 -0500

    embed short chunks directly summarize long chunks before embedding

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,13 @@
-use webhook_flows::{create_endpoint, request_handler, send_response};
+use webhook_flows::{ create_endpoint, request_handler, send_response };
 use vector_store_flows::*;
-use openai_flows::{
-    embeddings::{EmbeddingsInput},
-    OpenAIFlows,
-};
-use serde_json::{json, Value};
+use openai_flows::{ embeddings::{ EmbeddingsInput }, OpenAIFlows, chat };
+use serde_json::{ json, Value };
 use std::collections::HashMap;
 use std::str;
 use flowsnet_platform_sdk::logger;
 
-static CHAR_SOFT_LIMIT : usize = 20000;
-static CHAR_SOFT_MINIMUM : usize = 100;
+static CHAR_SOFT_LIMIT: usize = 20000;
+static CHAR_SOFT_MINIMUM: usize = 100;
 
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
@@ -23,8 +20,8 @@ async fn handler(_headers: Vec<(String, String)>, qry: HashMap<String, Value>, b
     logger::init();
     let collection_name = qry.get("collection_name").unwrap().as_str().unwrap();
     let vector_size = qry.get("vector_size").unwrap().as_str().unwrap();
-    let vector_size : u64 = vector_size.parse().unwrap();
-    let mut id : u64 = 0;
+    let vector_size: u64 = vector_size.parse().unwrap();
+    let mut id: u64 = 0;
 
     if qry.contains_key("reset") {
         log::debug!("Reset the database");
@@ -55,47 +52,42 @@ async fn handler(_headers: Vec<(String, String)>, qry: HashMap<String, Value>, b
     let mut openai = OpenAIFlows::new();
     openai.set_retry_times(3);
 
-    let s = str::from_utf8(&body).unwrap();
+    let str_vec_received = serde_json::from_slice::<Vec<String>>(&body).unwrap_or(vec![]);
+
+    let need_summarize_before_embed = str_vec_received.iter().any(|l| l.len() > CHAR_SOFT_LIMIT);
+
+    let mut s = str_vec_received.clone();
+
+    if need_summarize_before_embed {
+        s.clear();
+        for window in str_vec_received {
+            let sum = summarize_long_chunks(&window).await;
+            s.push(sum);
+        }
+    }
+
     let mut points = Vec::<Point>::new();
-    let mut current_section = String::new();
-    let mut is_code_block = false;
-    for line in s.lines() {
-        // Append the line to the current section if the current section is less than CHAR_SOFT_LIMIT
-        if current_section.len() < CHAR_SOFT_LIMIT {
-            current_section.push_str(line);
-            current_section.push('\n');
-        } else {
-            log::warn!("Section already has {} chars. Exceeded CHAR_SOFT_LIMIT. Skipped line: {}", current_section.len(), line);
-        }
-
-        if line.trim().starts_with("```") {
-            is_code_block = !is_code_block;
-        }
-
-        if line.trim().is_empty() && (!is_code_block) && (current_section.len() > CHAR_SOFT_MINIMUM) {
-            if !current_section.trim().is_empty() {
-                // create and save embedding
-                let input = EmbeddingsInput::String(current_section.clone());
-                match openai.create_embeddings(input).await {
-                    Ok(r) => {
-                        for v in r.iter() {
-                            let p = Point {
-                                id: PointId::Num(id),
-                                vector: v.iter().map(|n| *n as f32).collect(),
-                                payload: json!({"text": current_section}).as_object().map(|m| m.to_owned()),
-                            };
-                            points.push(p);
-                            log::debug!("Created vector {} with length {}", id, v.len());
-                            id += 1;
-                        }
-                    }
-                    Err(e) => {
-                        log::error!("OpenAI returned an error: {}", e);
-                    }
+    for line in s {
+        let input = EmbeddingsInput::String(line.clone());
+        match openai.create_embeddings(input).await {
+            Ok(r) => {
+                for v in r.iter() {
+                    let p = Point {
+                        id: PointId::Num(id),
+                        vector: v
+                            .iter()
+                            .map(|n| *n as f32)
+                            .collect(),
+                        payload: json!({"text": line}).as_object().map(|m| m.to_owned()),
+                    };
+                    points.push(p);
+                    log::debug!("Created vector {} with length {}", id, v.len());
+                    id += 1;
                 }
             }
-            // Start a new section
-            current_section.clear();
+            Err(e) => {
+                log::error!("OpenAI returned an error: {}", e);
+            }
         }
     }
     let points_count = points.len();
@@ -108,8 +100,18 @@ async fn handler(_headers: Vec<(String, String)>, qry: HashMap<String, Value>, b
 
     match collection_info(collection_name).await {
         Ok(ci) => {
-            log::debug!("There are {} vectors in collection `{}`", ci.points_count, collection_name);
-            send_success(&format!("Successfully inserted {} records. The collection now has {} records in total.", points_count, ci.points_count));
+            log::debug!(
+                "There are {} vectors in collection `{}`",
+                ci.points_count,
+                collection_name
+            );
+            send_success(
+                &format!(
+                    "Successfully inserted {} records. The collection now has {} records in total.",
+                    points_count,
+                    ci.points_count
+                )
+            );
         }
         Err(e) => {
             log::error!("Cannot get collection stat {}", e);
@@ -122,6 +124,30 @@ fn send_success(body: &str) {
     send_response(
         200,
         vec![(String::from("content-type"), String::from("text/html"))],
-        body.as_bytes().to_vec(),
+        body.as_bytes().to_vec()
     );
+}
+
+pub async fn summarize_long_chunks(input: &str) -> String {
+    let sys_prompt_1 = format!("You're a technical edtior bot.");
+    let co = chat::ChatOptions {
+        model: chat::ChatModel::GPT35Turbo16K,
+        system_prompt: Some(&sys_prompt_1),
+        restart: true,
+        temperature: Some(0.7),
+        max_tokens: Some(256),
+        ..Default::default()
+    };
+    let usr_prompt_1 = format!(
+        "To prepare for downstream question & answer task, you need to proprocess the source material, there are long chunks of text that are tool long to use as context, you need to extract the essence of such chunks, now please summarize this chunk: `{input}` into one concise paragraph, please stay truthful to the source material and handle the task in a factual manner."
+    );
+
+    let mut openai = OpenAIFlows::new();
+    openai.set_retry_times(2);
+
+    match openai.chat_completion("summarize-long-chunks", &usr_prompt_1, &co).await {
+        Ok(r) => r.choice,
+
+        Err(_e) => "".to_owned(),
+    }
 }
